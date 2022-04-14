17e2240dc48ca4fb83e5433bc89092d5e2857d55
yunfanshao <1504517387@qq.com>
2022-04-14 19:11:34 +0800

    Version 3
    Updated after the first afternoon conference.

--- WIN32-MingW/main_blinky.c
+++ WIN32-MingW/main_blinky.c
@@ -1,86 +1,3 @@
-/*
- * FreeRTOS V202112.00
- * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * http://www.FreeRTOS.org
- * http://aws.amazon.com/freertos
- *
- * 1 tab == 4 spaces!
- */
-
-/******************************************************************************
- * NOTE 1: Windows will not be running the FreeRTOS demo threads continuously, so
- * do not expect to get real time behaviour from the FreeRTOS Windows port, or
- * this demo application.  Also, the timing information in the FreeRTOS+Trace
- * logs have no meaningful units.  See the documentation page for the Windows
- * port for further information:
- * http://www.freertos.org/FreeRTOS-Windows-Simulator-Emulator-for-Visual-Studio-and-Eclipse-MingW.html
- *
- * NOTE 2:  This project provides two demo applications.  A simple blinky style
- * project, and a more comprehensive test and demo application.  The
- * mainCREATE_SIMPLE_BLINKY_DEMO_ONLY setting in main.c is used to select
- * between the two.  See the notes on using mainCREATE_SIMPLE_BLINKY_DEMO_ONLY
- * in main.c.  This file implements the simply blinky version.  Console output
- * is used in place of the normal LED toggling.
- *
- * NOTE 3:  This file only contains the source code that is specific to the
- * basic demo.  Generic functions, such FreeRTOS hook functions, are defined
- * in main.c.
- ******************************************************************************
- *
- * main_blinky() creates one queue, one software timer, and two tasks.  It then
- * starts the scheduler.
- *
- * The Queue Send Task:
- * The queue send task is implemented by the prvQueueSendTask() function in
- * this file.  It uses vTaskDelayUntil() to create a periodic task that sends
- * the value 100 to the queue every 200 milliseconds (please read the notes
- * above regarding the accuracy of timing under Windows).
- *
- * The Queue Send Software Timer:
- * The timer is an auto-reload timer with a period of two seconds.  The timer's
- * callback function writes the value 200 to the queue.  The callback function
- * is implemented by prvQueueSendTimerCallback() within this file.
- *
- * The Queue Receive Task:
- * The queue receive task is implemented by the prvQueueReceiveTask() function
- * in this file.  prvQueueReceiveTask() waits for data to arrive on the queue.
- * When data is received, the task checks the value of the data, then outputs a
- * message to indicate if the data came from the queue send task or the queue
- * send software timer.
- *
- * Expected Behaviour:
- * - The queue send task writes to the queue every 200ms, so every 200ms the
- *   queue receive task will output a message indicating that data was received
- *   on the queue from the queue send task.
- * - The queue send software timer has a period of two seconds, and is reset
- *   each time a key is pressed.  So if two seconds expire without a key being
- *   pressed then the queue receive task will output a message indicating that
- *   data was received on the queue from the queue send software timer.
- *
- * NOTE:  Console input and output relies on Windows system calls, which can
- * interfere with the execution of the FreeRTOS Windows port.  This demo only
- * uses Windows system call occasionally.  Heavier use of Windows system calls
- * can crash the port.
- */
-
 /* Standard includes. */
 #include <stdio.h>
 #include <conio.h>
@@ -92,51 +9,60 @@
 #include "semphr.h"
 
 /* Priorities at which the tasks are created. */
-#define	Frequency_Generation_PRIORITY	  ( 2 )
+#define	Frequency_VirtualISR_PRIORITY	  ( 3 )
 #define Frequency_Updater_PRIORITY		  ( 1 )
-#define Keyboard_Simulation_ISR_PRIORITY  ( 3 )
+#define Keyboard_Simulation_ISR_PRIORITY  ( 2 )
 
-/* The rate at which data is sent to the queue.  The times are converted from
-milliseconds to ticks using the pdMS_TO_TICKS() macro.
-#define mainTASK_SEND_FREQUENCY_MS			pdMS_TO_TICKS( 200UL )
-#define mainTIMER_SEND_FREQUENCY_MS			pdMS_TO_TICKS( 2000UL ) */
-
-/* The number of items the queue can hold at once.
-#define mainQUEUE_LENGTH					( 2 ) */
-
-/* The values sent to the queue receive task from the queue send task and the
-queue send software timer respectively.
-#define mainVALUE_SENT_FROM_TASK			( 100UL )
-#define mainVALUE_SENT_FROM_TIMER			( 200UL )
-*/
-/*-------------------------------------------------------
-
- * The tasks as described in the comments at the top of this file.
- */
-static void FrequencyUpdaterTask(  );
-static void FrequencyGenerationTask(  );
-static void KeyboardSimulationISRTask( );
+/* definition of task stack*/
+#define TASK_STACKSIZE                   2048
 
 
-/*-----------------------------------------------------------*/
+/*############Init Function###################*/
+void initDataStructs(void);
+void initCreateTasks(void);
+
+/*############Tasks###########################*/
+void FrequencyVirtualISRTask(  );
+void FrequencyUpdaterTask(  );
+void KeyboardSimulationISRTask( );
+void loadMangerTask( );
+
+/*###########Helper function##################*/
 int GetVirutalSystemTime ( int VirtualSystemTimeCount );
 
-/*-----------------------------------------------------------*/
+/*###########Timer Callback##################*/
+//void vTimer500MSCallback(xTimeHandle t_timer);
+
+
+
+/*###############################################
+ * ########Queue#################################
+ * ##############################################
+ */
 xQueueHandle frequencyQ;
 
-int VirtualSystemTimeCount = 0;
+/*##############################################
+ * ########Gobal Var############################
+ * #############################################
+ */
+double VirtualSystemTime = 0;
+double num[100] = {0};
 
+/*##############################################
+ * #######struct data type######################
+ * ############################################3
+ */
 struct frequency_time
 {
-	float frequency;
-	int   timestamp;
+	double frequency;
+	double   timestamp;
 } freq_SIM_ISR;
 
 struct frequency_ROC
 {
-	float freqData;
-	float rocData;
-	int timestamp;
+	double freqData;
+	double rocData;
+	double timestamp;
 };
 
 
@@ -146,58 +72,63 @@ struct frequency_ROC
 void main_blinky( void )
 {
 
-	/* Create the queue. */
-	frequencyQ = xQueueCreate( 100, sizeof(struct frequency_time  ) );
+	initDataStructs();
+	printf("Data struct has initialised!\n");
+	initCreateTasks();
+	printf("Tasks has initialised!\n");
 
-	if( frequencyQ != NULL )
-	{
-		/* Start the two tasks as described in the comments at the top of this
-		file. */
-		xTaskCreate(FrequencyUpdaterTask,			/* The function that implements the task. */
-					"Rx", 							/* The text name assigned to the task - for debug only as it is not used by the kernel. */
-					2048, 		/* The size of the stack to allocate to the task. */
-					NULL, 							/* The parameter passed to the task - not used in this simple case. */
-					Frequency_Updater_PRIORITY,/* The priority assigned to the task. */
-					NULL );							/* The task handle is not required, so NULL is passed. */
 
-		xTaskCreate( FrequencyGenerationTask, "TX", 2048, NULL, Frequency_Generation_PRIORITY, NULL );
-		//xTaskCreate( KeyboardSimulationISRTask, "Tn", 2048, NULL, Keyboard_Simulation_ISR_PRIORITY, NULL );
+	/* Start the tasks. */
+	vTaskStartScheduler();
 
-		/* Start the tasks and timer running. */
-		vTaskStartScheduler();
-	}
-
-	/* If all is well, the scheduler will now be running, and the following
-	line will never be reached.  If the following line does execute, then
-	there was insufficient FreeRTOS heap memory available for the idle and/or
-	timer tasks	to be created.  See the memory management section on the
-	FreeRTOS web site for more details. */
 	for( ;; );
 }
 /*-----------------------------------------------------------*/
 
-int GetVirtualSystemTime ( int VirtualSystemTimeCount ){
-	return VirtualSystemTimeCount * 20;
+void initDataStructs(void){
+	frequencyQ = xQueueCreate( 100, sizeof(struct frequency_time  ) );
+	return;
 }
 
-static void FrequencyGenerationTask(  )
+
+void initCreateTasks(void){
+	xTaskCreate( FrequencyVirtualISRTask, "FrequencyVirtualISRTask", TASK_STACKSIZE , NULL, Frequency_VirtualISR_PRIORITY, NULL );
+	xTaskCreate( FrequencyUpdaterTask, "FrequencyUpdaterTask", TASK_STACKSIZE , NULL, Frequency_Updater_PRIORITY, NULL );
+	return;
+}
+
+
+int GetVirtualSystemTime ( int NI ){
+	VirtualSystemTime = VirtualSystemTime + NI / 16;
+	return VirtualSystemTime;
+}
+
+void FrequencyVirtualISRTask(  )
 {
+	for (int i = 0; i < 100; i++){
+		num[i] = ( rand() % (400-266 + 1) + 266 );
+	}
+	for (int i = 0; i < 100; i++){
+		printf("num[%d] is %lf\n", i, num[i]);
+	}
+
 	while(1){
-		int num = ( rand() % (320-300 + 1) + 300 );
- 	 	freq_SIM_ISR.frequency = 16000 / (double)num;
- 	 	freq_SIM_ISR.timestamp = GetVirtualSystemTime(VirtualSystemTimeCount);
- 	 	VirtualSystemTimeCount++;
- 	 	printf("\n---------------------------------\n");
- 	 	printf("frequency is %lf \n", freq_SIM_ISR.frequency);
- 	 	printf("timestamp is %d \n", freq_SIM_ISR.timestamp);
- 	 	printf("\n---------------------------------\n");
-		xQueueSendToFront(frequencyQ, &freq_SIM_ISR, 0 );
-		vTaskDelay(20000);
+		for (int j = 0; j < 100; j++){
+			freq_SIM_ISR.frequency = 16000 / (double)num[j];
+ 	 		freq_SIM_ISR.timestamp = GetVirtualSystemTime(num[j]);
+
+ 	 		printf("\n---------------------------------\n");
+ 	 		printf("frequency is %lf \n", freq_SIM_ISR.frequency);
+ 	 		printf("timestamp is %lf \n", freq_SIM_ISR.timestamp);
+ 	 		printf("\n---------------------------------\n");
+ 	 		xQueueSendToFront(frequencyQ, &freq_SIM_ISR, 0 );
+ 	 		vTaskDelay(2000);
+		}
 	}
 }
 /*-----------------------------------------------------------*/
 
-static void FrequencyUpdaterTask( )
+void FrequencyUpdaterTask( )
 {
 		float freqValNew = 0;
 		float freqValOld = 0;
@@ -219,23 +150,23 @@ static void FrequencyUpdaterTask( )
 					freqValOld = freqValNew;
 					continue;
 				}
-				printf("FreqValNew is %f \nFreqValOld is %f\n ----\n", freqValNew, freqValOld);
+				printf("FreqValNew is %lf \nFreqValOld is %lf\n ----\n", freqValNew, freqValOld);
 				//rate of change equation from cs723 assignment 1 brief 2022
 				roc = ((freqValNew - freqValOld) * 16000) / ((16000/freqValNew + 16000/freqValOld) / 2 );
 				freqValOld = freqValNew;
 				frequency_ROC.freqData = freqValNew;
 				frequency_ROC.rocData = roc;
 				frequency_ROC.timestamp = ReceiveGenerateFre.timestamp;
-				printf("FreData is %f\n", freqValNew);
-				printf("ROC is %f \n", roc);
-				printf("Timestamp is %d \n", frequency_ROC.timestamp);
+				printf("FreData is %lf\n", freqValNew);
+				printf("ROC is %lf \n", roc);
+				printf("Timestamp is %lf \n", frequency_ROC.timestamp);
 				printf("\n !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \n");
 			}
 			fflush( stdout );
-			vTaskDelay(2000);
+			vTaskDelay(200);
 		}
 }
-/*-----------------------------------------------------------*/
+/*-----------------------------------------------------------
 static void KeyboardSimulationISRTask( ){
 	float thre_fre = 0;
 	float thre_roc = 0;
@@ -267,3 +198,4 @@ static void KeyboardSimulationISRTask( ){
 		vTaskDelay(200);
 	}
 }
+*/
